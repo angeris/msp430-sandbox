da33dd219fed0fdfc2e61e51951294fa9b3ca1cb
Guillermo Angeris <guillean@stanford.edu>
2017-03-10 19:10:53 -0800

    blinky! and other stuff

---
+++ blinky/blinky.c
@@ -0,0 +1,12 @@
+#include <msp430.h>
+
+void main() {
+    WDTCTL = WDTPW | WDTHOLD;
+    P1DIR = 0x01; // Set the pin to be an output
+
+    while(1) {
+        volatile unsigned long i = 50000;
+        P1OUT ^= 0x01; // Switch pin's level
+        while(i--);
+    }
+}
