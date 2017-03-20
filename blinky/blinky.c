b073c6ac7527dfec182b80892c055622d9b39c9d
Guillermo Angeris <guillean@stanford.edu>
2017-03-20 01:28:57 -0700

    more comments

--- blinky/blinky.c
+++ blinky/blinky.c
@@ -1,7 +1,7 @@
 #include <msp430.h>
 
 void main() {
-    WDTCTL = WDTPW | WDTHOLD;
+    WDTCTL = WDTPW | WDTHOLD; // Disable the watchdog timer
     P1DIR = 0x01; // Set the pin to be an output
 
     while(1) {
